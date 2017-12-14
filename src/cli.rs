e5c9c20f249ed5fad293f0af5d12e429a41af84f
ryym <ryym.64@gmail.com>
2017-12-14 22:25:51 +0900

    Tweak command line help message

--- src/cli.rs
+++ src/cli.rs
@@ -74,7 +74,7 @@ fn parse_args(args: &Vec<String>) -> Result<Parsed> {
 
 fn define_opts(opts: &mut Options) -> &mut Options {
     opts.optflag("h", "help", "print this help message");
-    opts.optflag("r", "run", "run given code using Rust Playground");
+    opts.optflag("r", "run", "compile and run given code using Rust Playground");
     opts.optflag("o", "open", "open Rust Playground with given code");
     opts.optopt("c", "channel",
                 "chose release channel which compiles code",
