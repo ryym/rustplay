8f21a628c2326723268e72128106275678c51c18
ryym <ryym.64@gmail.com>
2017-12-16 00:08:47 +0900

    Improve new line output handling

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,20 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let ret = rustplay::cli::run(&args);
     match ret {
-        Ok(Some(msg)) => println!("{}", msg),
+        Ok(Some(mut msg)) =>
+            print!("{}", ensure_newline(&mut msg)),
         Ok(None) => {},
         Err(err) => {
-            println!("{}", err.description());
+            let mut msg = err.description().to_string();
+            print!("{}", ensure_newline(&mut msg));
             process::exit(1);
         },
     }
+}
+
+fn ensure_newline(s: &mut String) -> &mut String {
+    if !s.ends_with('\n') {
+        s.push_str("\n");
+    }
+    s
 }
